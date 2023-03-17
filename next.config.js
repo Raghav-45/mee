520b6f88af4f8a503884c4e047780a0abf5b3e09
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-03-17 22:17:25 +0530

    Added PWA ( Progressive Web App )

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  register: true,
+  skipWaiting: true,
+  dest: "public",
+  disable:
+    process.env.NODE_ENV === "development"
+    // process.env.NODE_ENV === "preview"
+    // process.env.NODE_ENV === "production"
+  // delete two lines above to enable PWA in production deployment
+});
+
+module.exports = withPWA({
+  // swcMinify: true,
+  // reactStrictMode: true,
+  eslint: {
+    dirs: ["src"],
+  },
+});
