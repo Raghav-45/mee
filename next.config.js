9fbec765ba692c3fb103c53167633ea8b1529fc9
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-03-17 22:55:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,11 @@
 //   reactStrictMode: true,
 // }
 
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest:"public",
-    register:true,
-    skipWaiting:true,
-    disable:process.env.NODE_ENV === 'development'
-  }
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
