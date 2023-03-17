f9ad9e522ed0ba92cfd4c6a9110d6f8903479b07
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-03-17 22:35:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   // swcMinify: true,reactStrictMode: true,
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development"
+//   }
+//   // eslint: {
+//   //   dirs: ["src"],
+//   // },
+// });
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
-  // swcMinify: true,reactStrictMode: true,
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
-  }
-  // eslint: {
-  //   dirs: ["src"],
-  // },
-});
+  // next.js config
+})
