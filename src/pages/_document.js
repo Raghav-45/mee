7334d12c4d0bf59071fc5db0e8e7c5a5d11fbac6
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-04-02 01:08:13 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="MEV" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="theme-color" content="#000" />
