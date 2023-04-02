c2afce390302e6630c20f00e67084d7c686fb66c
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-04-02 23:05:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  // reactStrictMode: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+// module.exports = withPWA({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   // reactStrictMode: true,
+//   disable: process.env.NODE_ENV === 'development',
+// });
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/cats',
+        destination: 'https://meowfacts.herokuapp.com/' // Proxy to Backend
+      }
+    ]
+  }
+}
