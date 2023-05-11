48f24f306588268556f1c08fd592ea23c9302234
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-05-11 17:02:55 +0530

    adding Google Maps

--- src/pages/map.js
+++ src/pages/map.js
@@ -14,80 +14,31 @@ import { TbTruckLoading } from 'react-icons/tb'
 import { Toast } from "../../components/Toast";
 import { supabase } from '../../lib/supabaseClient'
 import { useAuth } from '../../contexts/AuthContext'
-// import Map from "mapmyindia-react"
 
-// const map = new mappls.Map('map', {center:{lat:28.612964,lng:77.229463} });
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
 
 export default function Home() {
   const toast = useToast()
   const { currentUser } = useAuth()
 
-  const [isMapSDKLoaded, setIsMapSDKLoaded] = useState(false)
-  const [isMapPluginLoaded, setIsMapPluginLoaded] = useState(false)
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyB0f0o77WzVWMIXX69u0oJL8zyKPKSsAEA',
+  })
+
+  const center = { lat: 28.659051, lng: 77.113777 }
 
   const [pickupLoc, setPickupLoc] = useState('')
   const [destinationLoc, setDestinationLoc] = useState('')
-  const MapSettings = {
-    zoomControl: false,
-    zoom: 16,
-    hybrid: true,
+
+  if (!isLoaded) {
+    return <Box>Loading...</Box>
   }
-
-  var map
-  function initMap() {
-    map = new mappls.Map('map', {center:[28.667936, 77.112228], zoom: 16, search: false})
-
-    // map.addListener('load', function () {
-    //   var from= new mappls.Marker({
-    //     map: map,
-    //     position: {"lat": 28.659051 ,"lng":77.113777},
-    //     fitbounds: true,
-    //     icon: "https://maps.mapmyindia.com/images/from.png"
-    //   })
-      
-    //   var to= new mappls.Marker({
-    //     map: map,
-    //     position: {"lat": 28.667936 ,"lng":77.112228},
-    //     fitbounds: true,
-    //     icon: "https://maps.mapmyindia.com/images/to.png"
-    //   })
-    // })
-  }
-
-  function addMarker() {
-    map.addListener('load', function () {
-      var from= new mappls.Marker({
-        map: map,
-        position: {"lat": 28.659051 ,"lng":77.113777},
-        fitbounds: true,
-        icon: "https://maps.mapmyindia.com/images/from.png"
-      })
-      
-      var to= new mappls.Marker({
-        map: map,
-        position: {"lat": 28.667936 ,"lng":77.112228},
-        fitbounds: true,
-        icon: "https://maps.mapmyindia.com/images/to.png"
-      })
-
-      // mappls.getDistance({
-      //   coordinates: "518NSV;123ZRR",
-      // },function callback (data) {
-      //   console.log("Data",data);
-      // })
-    })
-  }
-
-  useEffect(() => {
-    isMapSDKLoaded && initMap()
-    isMapSDKLoaded && isMapPluginLoaded && addMarker()
-  }, [isMapSDKLoaded, isMapPluginLoaded])
 
   return (
-    <Box>
-      <Script src="https://apis.mappls.com/advancedmaps/api/06eb2afcbe49d1fdfe37e89dbe823094/map_sdk?layer=vector&v=3.0" onLoad={() => setIsMapSDKLoaded(true)} />
-      <Script src="https://apis.mappls.com/advancedmaps/api/6184180e-dc53-4a44-a7d1-08795a1c70a6/map_sdk_plugins?v=3.0" onLoad={() => setIsMapPluginLoaded(true)} />
-      <div id="map"></div>
+    <Box h={'100%'} w={'100%'}>
+      <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}}>
+
+      </GoogleMap>
     </Box>
   )
 }
