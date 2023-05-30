e8d0af08bebdc575042d9393a7308fc279430e18
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-05-30 21:14:58 +0530

    Update OnBoarding.js

--- components/OnBoarding.js
+++ components/OnBoarding.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Box, Button, Center, Container, Flex, Heading, Text } from '@chakra-ui/react'
 import { FaBolt } from 'react-icons/fa'
 import { HiArrowRight } from 'react-icons/hi'
+import { useAuth } from '../contexts/AuthContext'
 
 export const OnBoarding = (props) => {
+  const { currentUser } = useAuth()
   return (
-    <Container maxW='container.lg' h={'85vh'} justifyContent={'space-between'} centerContent>
+    <Container maxW='container.lg' h={'80vh'} justifyContent={'space-between'} centerContent>
       <Flex alignItems={'center'} flexGrow={1} flexShrink={1} flexBasis={'auto'}>
         <Flex h={'200px'} w={'200px'} justifyContent={'center'} alignItems={'center'} rounded={'full'} border='1px' borderColor='gray.500'>
           <Flex h={'calc(100% - 80px + 4px)'} w={'calc(100% - 80px + 4px)'} justifyContent={'center'} alignItems={'center'} rounded={'full'} border='2px' borderColor='gray.700'>
